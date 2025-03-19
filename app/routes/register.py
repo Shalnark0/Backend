0091f638c90f6e1f7afac2798975a853fcae0e66
Николя <forlatotskiy@gmail.com>
2025-03-19 14:24:04 +0300

    commit

--- app/routes/register.py
+++ app/routes/register.py
@@ -5,3 +5,4 @@ register_routes = Blueprint("register", __name__, url_prefix="/user")
 
 register_routes.route("/register", methods=["POST"])(RegisterController.register)
 register_routes.route("/upload_avatar/<string:user_id>", methods=["POST"])(RegisterController.upload_avatar)
+register_routes.route("/get_avatar/<string:user_id>", methods=["GET"])(RegisterController.get_avatar)
